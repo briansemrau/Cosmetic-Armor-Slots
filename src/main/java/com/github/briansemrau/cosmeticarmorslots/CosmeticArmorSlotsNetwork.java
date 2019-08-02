e8c87b537881c351bdc1f449ef0031c22df3ccc9
Brian Semrau <brian.semrau@gmail.com>
2019-08-01 20:37:26 -0400

    Fixed dedicated server crash
    As it turns out, the problem is that I'm an idiot.

--- src/main/java/com/github/briansemrau/cosmeticarmorslots/CosmeticArmorSlotsNetwork.java
+++ src/main/java/com/github/briansemrau/cosmeticarmorslots/CosmeticArmorSlotsNetwork.java
@@ -4,11 +4,10 @@ import com.github.briansemrau.cosmeticarmorslots.interfaces.IPlayerEntityMixin;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
-import net.minecraft.client.network.packet.CustomPayloadS2CPacket;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
+import net.minecraft.network.Packet;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.PacketByteBuf;
 
@@ -20,6 +19,15 @@ public class CosmeticArmorSlotsNetwork {
     public static final Identifier VISIBILITY_UPDATE_PACKET = new Identifier(MOD_ID, "visibility_update");
 
     public static void onInitialize() {
+        ServerSidePacketRegistry.INSTANCE.register(VISIBILITY_UPDATE_PACKET, (packetContext, packetByteBuf) -> {
+            EquipmentSlot slot = packetByteBuf.readEnumConstant(EquipmentSlot.class);
+            boolean visible = packetByteBuf.readBoolean();
+
+            ((IPlayerEntityMixin) packetContext.getPlayer()).setUseCosmeticArmorSlot(slot.getEntitySlotId(), visible);
+        });
+    }
+
+    public static void onInitializeClient() {
         ClientSidePacketRegistry.INSTANCE.register(EQUIP_COSMETIC_ARMOR_PACKET, (packetContext, packetByteBuf) -> {
             int entityId = packetByteBuf.readVarInt();
             EquipmentSlot slot = packetByteBuf.readEnumConstant(EquipmentSlot.class);
@@ -32,28 +40,22 @@ public class CosmeticArmorSlotsNetwork {
                 ((IPlayerEntityMixin) player).setUseCosmeticArmorSlot(slot.getEntitySlotId(), useCosmeticSlot);
             }
         });
-        ServerSidePacketRegistry.INSTANCE.register(VISIBILITY_UPDATE_PACKET, (packetContext, packetByteBuf) -> {
-            EquipmentSlot slot = packetByteBuf.readEnumConstant(EquipmentSlot.class);
-            boolean visible = packetByteBuf.readBoolean();
-
-            ((IPlayerEntityMixin) packetContext.getPlayer()).setUseCosmeticArmorSlot(slot.getEntitySlotId(), visible);
-        });
     }
 
-    public static CustomPayloadS2CPacket createEquipCosmeticArmorPacket(int entityId, EquipmentSlot slot, ItemStack stack, boolean visible) {
+    public static Packet<?> createEquipCosmeticArmorPacket(int entityId, EquipmentSlot slot, ItemStack stack, boolean visible) {
         PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
         buf.writeVarInt(entityId);
         buf.writeEnumConstant(slot);
         buf.writeItemStack(stack);
         buf.writeBoolean(visible);
-        return new CustomPayloadS2CPacket(EQUIP_COSMETIC_ARMOR_PACKET, buf);
+        return ServerSidePacketRegistry.INSTANCE.toPacket(EQUIP_COSMETIC_ARMOR_PACKET, buf);
     }
 
-    public static CustomPayloadC2SPacket createCosmeticSlotVisibilityUpdatePacket(EquipmentSlot slot, boolean visible) {
+    public static Packet<?> createCosmeticSlotVisibilityUpdatePacket(EquipmentSlot slot, boolean visible) {
         PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
         buf.writeEnumConstant(slot);
         buf.writeBoolean(visible);
-        return new CustomPayloadC2SPacket(VISIBILITY_UPDATE_PACKET, buf);
+        return ClientSidePacketRegistry.INSTANCE.toPacket(VISIBILITY_UPDATE_PACKET, buf);
     }
 
 }
